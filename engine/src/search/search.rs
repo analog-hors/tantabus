b4085349ab51126ee4b6329be45b51a765e9ee09
analog-hors <44050761+analog-hors@users.noreply.github.com>
2021-12-11 11:55:54 +1000

    Tweak LMP again

--- engine/src/search/search.rs
+++ engine/src/search/search.rs
@@ -54,7 +54,7 @@ fn lmr_calculate_reduction(i: usize) -> u8 {
 
 fn lmp_quiets_to_check(depth: u8) -> usize {
     let depth = depth as usize;
-    7 + depth * depth * 4
+    10 + depth * depth * 4
 }
 
 impl<H: SearchHandler> Searcher<'_, H> {
