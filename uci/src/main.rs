0527e94deaa511120de4b2a460ee1f663a0ce9fd
analog-hors <44050761+analog-hors@users.noreply.github.com>
2021-12-28 10:54:51 +1000

    Version 1.0.2
    A quick fix to a serious bug where Tantabus could not checkmate in simple endgames. Otherwise identical to version 1.0.1.

--- uci/src/main.rs
+++ uci/src/main.rs
@@ -18,7 +18,7 @@ mod bench;
 use options::UciOptionsHandler;
 use convert::*;
 
-const VERSION: &str = "1.0.1";
+const VERSION: &str = "1.0.2";
 
 struct UciHandler {
     time_manager: StandardTimeManager,
