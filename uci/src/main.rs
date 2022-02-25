7f29734bc62c42e6359f732057bbb00fa4baf937
analog-hors <44050761+analog-hors@users.noreply.github.com>
2022-02-25 19:05:06 +1000

    Bump version

--- uci/src/main.rs
+++ uci/src/main.rs
@@ -18,7 +18,7 @@ mod bench;
 use options::UciOptionsHandler;
 use convert::*;
 
-const VERSION: &str = "2.0.0";
+const VERSION: &str = "3.0.0 dev";
 
 struct UciHandler {
     time_manager: StandardTimeManager,
