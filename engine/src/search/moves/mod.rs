e8c5014056ebdbb1affb653d258dfa8dbf5bc625
analog-hors <44050761+analog-hors@users.noreply.github.com>
2022-09-05 19:11:37 +1000

    what

--- engine/src/search/moves/mod.rs
+++ engine/src/search/moves/mod.rs
@@ -221,12 +221,14 @@ impl QSearchMoveList {
             capture_moves.to &= their_pieces;
             for mv in capture_moves {
                 // CITE: This use of SEE in quiescence and pruning moves with
-                // negative SEE was implemented based on a chesspgoramming.org page.
+                // negative SEE was implemented based on a chessprogramming.org page.
                 // https://www.chessprogramming.org/Quiescence_Search#Limiting_Quiescence
-                let eval = static_exchange_evaluation(board, mv);
-                if eval < Eval::ZERO {
+                if !static_exchange_evaluation_above::<0>(board, mv) {
                     continue;
                 }
+                let attacker = board.piece_on(mv.from).unwrap();
+                let victim = board.piece_on(mv.to).unwrap();
+                let eval = Eval::cp(victim as i16 * 256 - attacker as i16);
                 move_list.push((mv, MoveScore::Capture(eval)));
             }
             false
