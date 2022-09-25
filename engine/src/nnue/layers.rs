cbebd494c4e37fae18cc1f4dee1fc70911c0ec9a
analog-hors <44050761+analog-hors@users.noreply.github.com>
2022-09-25 20:07:06 +1000

    Implement explicitly vectorized dot product
    ELO   | 9.44 +- 6.03 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=8MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 6224 W: 1607 L: 1438 D: 3179

--- engine/src/nnue/layers.rs
+++ engine/src/nnue/layers.rs
@@ -1,3 +1,4 @@
+pub type LinearI = u8;
 pub type LinearW = i8;
 pub type LinearB = i32;
 
@@ -8,7 +9,7 @@ pub struct Linear<const INPUTS: usize, const OUTPUTS: usize> {
 }
 
 impl<const INPUTS: usize, const OUTPUTS: usize> Linear<INPUTS, OUTPUTS> {
-    pub fn activate(&self, inputs: &[LinearW; INPUTS], outputs: &mut [LinearB; OUTPUTS]) {
+    pub fn activate(&self, inputs: &[LinearI; INPUTS], outputs: &mut [LinearB; OUTPUTS]) {
         *outputs = self.biases;
         for (o, w) in outputs.iter_mut().zip(&self.weights) {
             *o += dot_product(inputs, w);
@@ -38,7 +39,49 @@ impl<const INPUTS: usize, const OUTPUTS: usize> BitLinear<INPUTS, OUTPUTS> {
     }
 }
 
-fn dot_product<const LEN: usize>(vec: &[LinearW; LEN], other: &[LinearW; LEN]) -> LinearB {
+fn dot_product<const LEN: usize>(vec: &[LinearI; LEN], other: &[LinearW; LEN]) -> LinearB {
+    use std::arch::x86_64::*;
+
+    #[cfg(target_feature = "avx2")] {
+        const VEC_SIZE: usize = std::mem::size_of::<__m256i>();
+        // lmao rip if this isn't true
+        if LEN % VEC_SIZE == 0 {
+            unsafe {
+                let mut sum = _mm256_setzero_si256(); // i32x8
+                for (l, r) in vec.chunks_exact(VEC_SIZE).zip(other.chunks_exact(VEC_SIZE)) {
+                    let l = _mm256_loadu_si256(l.as_ptr() as *const __m256i);
+                    let r = _mm256_loadu_si256(r.as_ptr() as *const __m256i);
+
+                    // u8x32 * i8x32 -> i16x32 horizontal add -> i16x16
+                    let partial = _mm256_maddubs_epi16(l, r);
+                    // i16x16 * i16x16 -> i32x16 horizontal add -> i32x8
+                    // We only want the horizontal add, so we no-op multiply with 1
+                    let partial = _mm256_madd_epi16(partial, _mm256_set1_epi16(1));
+                    // i32x8 + i32x8 -> i32x8
+                    sum = _mm256_add_epi32(sum, partial);
+                }
+
+                // Sum i32x8 to i32.
+                // i32x8 lower half -> i32x4
+                let lower = _mm256_castsi256_si128(sum);
+                // i32x8 upper half -> i32x4
+                let upper = _mm256_extracti128_si256::<1>(sum);
+                // i32x4 + i32x4 -> i32x4
+                let sum = _mm_add_epi32(lower, upper);
+                // i32x4 reversed -> i32x4
+                let reversed = _mm_shuffle_epi32(sum, 0b_00_01_10_11);
+                // i32x4 + i32x4 reversed -> i32x2 + ...
+                let sum = _mm_add_epi32(sum, reversed);
+                // i32x2 + ... element 0 -> i32
+                let lower = _mm_cvtsi128_si32(sum);
+                // i32x2 + ... element 1 -> i32
+                let upper = _mm_extract_epi32::<1>(sum);
+                return lower + upper;
+            }
+        }
+    }
+
+    // Fallback impl
     vec.iter().zip(other).map(|(&v, &o)| v as LinearB * o as LinearB).sum()
 }
 
