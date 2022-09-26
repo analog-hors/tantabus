8e5e08ae0222f7362ac1fa319104b4d688ca764a
analog-hors <44050761+analog-hors@users.noreply.github.com>
2022-09-26 14:42:09 +1000

    Implement capture history

--- engine/src/search/helpers.rs
+++ engine/src/search/helpers.rs
@@ -1,10 +1,9 @@
 use cozy_chess::*;
 
+pub fn move_is_capture(mv: Move, board: &Board) -> bool {
+    board.colors(!board.side_to_move()).has(mv.to)
+}
+
 pub fn move_is_quiet(mv: Move, board: &Board) -> bool {
-    let color = board.side_to_move();
-    let mut capture_squares = board.colors(!color);
-    if let Some(ep) = board.en_passant() {
-        capture_squares |= Square::new(ep, Rank::Third.relative_to(!color)).bitboard();
-    }
-    !capture_squares.has(mv.to) && mv.promotion.is_none()
+    !move_is_capture(mv, board) && mv.promotion.is_none()
 }
